cff11c20af7a1efc27021f5b17b69b44680ed1e9
Raí Carvalho Gondim <raicg2@gmail.com>
2018-11-12 17:25:30 -0300

    Now acquiring title, subtitle and notice

--- notices_that_i_want_to_read/spiders/notices_that_i_want_to_read.py
+++ notices_that_i_want_to_read/spiders/notices_that_i_want_to_read.py
@@ -13,10 +13,20 @@ class notices_that_i_want_to_read(Spider):
         table_with_subtitles = response.xpath("//div[contains(@class , 'feed-post-body-resumo')]")
 
         title = table_with_notices.xpath('./text()').extract()
+        href = table_with_notices.xpath('./@href').extract()
         subtitle = table_with_subtitles.xpath('./div[contains(@class , _label_event)]/text()').extract()
 
-        for i in range(len(title)):
-            loader = ItemLoader(item=NoticesThatIWantToReadItem(), response=response)
-            loader.add_value('title', title[i])
-            loader.add_value('subtitle', subtitle[i])
-            yield loader.load_item()
+        for i in range(len(href)):
+            yield response.follow(href[i], callback=self.parse_notice_page,
+                meta={'title': title[i], 'subtitle': subtitle[i]}
+            )
+
+    def parse_notice_page(self, response):
+        notice = response.xpath("//p[contains(@class, 'content-text__container')]/text()").extract()
+
+        loader = ItemLoader(item=NoticesThatIWantToReadItem(), response=response)
+        loader.add_value('title', response.meta.get('title'))
+        loader.add_value('subtitle', response.meta.get('subtitle'))
+        loader.add_value('notice', notice)
+
+        return loader.load_item()
